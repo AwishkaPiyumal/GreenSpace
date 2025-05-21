6a058191278a12ac30e4f7bc0abd38fb8c191e6d
Piyumal <139644276+AwishkaPiyumal@users.noreply.github.com>
2025-05-21 16:38:39 +0530

    Create HomeController.java

---
+++ backend/src/main/java/com/piyumal/ecommerce/store/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.piyumal.ecommerce.store.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController {
+
+    @GetMapping
+
+    public String HomeControllerHandler() {
+        return "Welcome to GreenSpace";
+    }
+
+}
